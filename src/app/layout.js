940a2535146803fbdfa1c443ce3b53c5788d470e
mokaradag <mokaradag@gmail.com>
2026-07-21 16:49:54 +0300

    Remove external font dependency for offline use

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,12 +12,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="tr">
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Geist:wght@400;500;600;700&family=Geist+Mono:wght@400;500&display=swap"
-          rel="stylesheet"
-        />
         <link rel="icon" type="image/svg+xml" href={FAVICON} />
       </head>
       <body className="theme-dark">{children}</body>
