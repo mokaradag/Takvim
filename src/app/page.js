5d785023c911c8b43a856699c281ad7fca404436
mokaradag <mokaradag@gmail.com>
2026-07-22 18:45:53 +0300

    Uygulama tarih gösterimini gün ay yıl olarak ayarla

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,8 @@
 'use client';
 import dynamic from 'next/dynamic';
+import { setAppDateDisplayFormat } from '../scheduling/dates';
+
+setAppDateDisplayFormat('dd/mm/yyyy');
 
 const App = dynamic(() => import('../components/shell/ApplicationRoot'), { ssr: false });
 
