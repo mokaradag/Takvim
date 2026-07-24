ebb3416857c4f09e491a14294298e9fefe4370ff
mokaradag <mokaradag@gmail.com>
2026-07-24 19:07:37 +0300

    Read Actual session capabilities transactionally

--- src/app/api/mergen-rota/session/route.js
+++ src/app/api/mergen-rota/session/route.js
@@ -1,4 +1,5 @@
 import { createSqlAppRepository } from '../../../../server/repository/sqlAppRepository.js';
+import { sql, withSqlTransaction } from '../../../../server/db/pool.js';
 import { safeErrorResponse } from '../../../../server/errors.js';
 
 export const runtime = 'nodejs';
@@ -7,7 +8,11 @@ export const revalidate = 0;
 
 export async function GET() {
   try {
-    return Response.json(await createSqlAppRepository().loadSessionContext(), {
+    const session = await withSqlTransaction(
+      () => createSqlAppRepository().loadSessionContext(),
+      { isolationLevel: sql.ISOLATION_LEVEL.SERIALIZABLE }
+    );
+    return Response.json(session, {
       headers: { 'cache-control': 'no-store' }
     });
   } catch (error) {
