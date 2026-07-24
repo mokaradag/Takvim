7b9f340cdf8ea49df2e85d52e1c521caa89a95ee
mokaradag <mokaradag@gmail.com>
2026-07-24 10:43:19 +0300

    Allow explicit SQL transaction isolation

--- src/server/db/pool.js
+++ src/server/db/pool.js
@@ -24,14 +24,14 @@ export async function getSqlPool() {
   return poolPromise;
 }
 
-export async function withSqlTransaction(work) {
+export async function withSqlTransaction(work, { isolationLevel = sql.ISOLATION_LEVEL.READ_COMMITTED } = {}) {
   const activeTransaction = transactionContext.getStore();
   if (activeTransaction) return work(activeTransaction, sql);
 
   const pool = await getSqlPool();
   const transaction = new sql.Transaction(pool);
   try {
-    await transaction.begin(sql.ISOLATION_LEVEL.READ_COMMITTED);
+    await transaction.begin(isolationLevel);
     return await transactionContext.run(transaction, async () => {
       const result = await work(transaction, sql);
       await transaction.commit();
