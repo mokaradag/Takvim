05801ae76a555cd80725fdbd8991871ff7337143
mokaradag <mokaradag@gmail.com>
2026-07-24 10:18:03 +0300

    Return complete assignees for visible tasks

--- src/app/api/mergen-rota/snapshot/route.js
+++ src/app/api/mergen-rota/snapshot/route.js
@@ -1,4 +1,4 @@
-import { createSqlAppRepository } from '../../../../server/repository/sqlAppRepository.js';
+import { createProjectedSqlAppRepository } from '../../../../server/repository/projectedSqlAppRepository.js';
 import { safeErrorResponse } from '../../../../server/errors.js';
 
 export const runtime = 'nodejs';
@@ -7,7 +7,7 @@ export const revalidate = 0;
 
 export async function GET() {
   try {
-    return Response.json(await createSqlAppRepository().loadSnapshot(), {
+    return Response.json(await createProjectedSqlAppRepository().loadSnapshot(), {
       headers: { 'cache-control': 'no-store' }
     });
   } catch (error) {
