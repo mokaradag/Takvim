3bf50b220a260d8cf765d19cdaee1a595eac21fa
mokaradag <mokaradag@gmail.com>
2026-07-24 23:09:26 +0300

    Keep native SQL drivers external to Next.js bundles

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    serverComponentsExternalPackages: ['mssql', 'msnodesqlv8'],
+  },
 };
 
 export default nextConfig;
